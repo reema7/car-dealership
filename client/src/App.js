1e642cba3d90b60b4884da0dc230786a53635a0b
Reema Bajwa <reema.bajwa@gmail.com>
2025-08-11 23:05:14 -0700

    fix error in api

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ const useAuth = () => {
 };
 
 // API Base URL (change to your server URL)
-const API_BASE = 'https://car-dealership-xmlx-ps86to0co-reemas-projects-55d93993.vercel.app/';
+const API_BASE = 'https://car-dealership-xmlx-ps86to0co-reemas-projects-55d93993.vercel.app/api';
 
 // Utility functions for WebAuthn
 const base64urlToBuffer = (base64url) => {
